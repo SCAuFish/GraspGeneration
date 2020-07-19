99348a22bb0f504e7b38a483f46b47ffbc6f6782
AuFish <shenchg126@gmail.com>
2020-07-18 23:16:49 -0700

    containing all make files

---
+++ grasp_src/CMakeFiles/grasp_gen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grasp_gen.dir/PointCloud.cu.o"
+  "CMakeFiles/grasp_gen.dir/grasp_gen.cpp.o"
+  "grasp_gen"
+  "grasp_gen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/grasp_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ grasp_src/CMakeFiles/smooth_normals.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smooth_normals.dir/smooth_normals.cpp.o"
+  "smooth_normals"
+  "smooth_normals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smooth_normals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

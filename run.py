550c784e3dbd58b06007ee0aff75676b683a870f
AuFish <shenchg126@gmail.com>
2020-10-26 18:33:52 -0700

    skip objects with generated grasps

--- run.py
+++ run.py
@@ -41,9 +41,10 @@ for i in range(len(object_list)):
     seeds = [1]
     for i in range(len(seeds)):
         objDir = "{}_{}".format(objId, seeds[i])
-        # if os.path.isdir("{}/{}".format(CLOUD_DIR, objDir)):
+        if os.path.exists("{}/{}/filtered.out".format(CLOUD_DIR, objDir)):
             # skip generated grasps
-        #     continue
+            print(f"skipped generation of {objDir}")
+            continue
         if not args.skip_pcd:
             # generate_point_cloud(objId, seeds[i], dataset_dir = ASSET_DIR, render_collision=True)
             exit_code = os.system("python3 pcloud_src/data_generation.py {} {} {}".format(objId, seeds[i], ASSET_DIR))
